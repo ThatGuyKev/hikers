096ad3b7308474f41ce3bb9b12139a8a79afded6
Ahmed Abdalla <ahmed_aallah@hotmail.com>
2021-02-06 15:21:27 +0400

    feat: Add RouteCard structure

---
+++ src/components/RouteCard/RouteCard.style.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components/native';
+
+export const CardContainer = styled.View``;
+
+export const TitleWrapper = styled.View``;
+
+export const Title = styled.Text``;
+
+export const ImageWrapper = styled.View``;
+
+export const DescWrapper = styled.View``;
+
+export const Desc = styled.Text``;
+
+export const DiffWrapper = styled.View``;
+
+export const Difficulty = styled.Text``;
+
+export const DurWrapper = styled.View``;
+
+export const Duration = styled.Text``;
